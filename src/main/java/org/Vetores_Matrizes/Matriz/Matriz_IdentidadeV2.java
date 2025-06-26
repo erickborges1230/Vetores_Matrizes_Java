02bd3c78f49cf3a8fd1758d5a3f006b4d1333dff
Erick Borges <erickborges1230@gmail.com>
2025-06-25 22:52:41 -0300

    first commit

---
+++ src/main/java/org/Vetores_Matrizes/Matriz/Matriz_IdentidadeV2.java
@@ -0,0 +1,33 @@
+package org.Vetores_Matrizes.Matriz;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Matriz_IdentidadeV2 {
+    public static void main(String[] args) throws IOException {
+        //Contrua uma matriz Identidade I=3 (apenas os valores diagonais são = 1) todos os outros são zero
+        int i, j;
+        int I[][] = new int[3][3];
+        //Arquivos para guardar as informações da Matriz identidade
+        FileWriter arq = new FileWriter("Matriz.txt");
+        PrintWriter gravaarq = new PrintWriter(arq);
+
+        for(i=0; i<3; i++)
+            for (j=0; j<3;j++){
+                if (i==j)
+                    I[i][j] = 1;
+                else
+                    I[i][j] = 0;
+            }
+        for (i=0;i<3;i++)
+        {
+            for (j=0;j<3;j++)
+                gravaarq.print("==========================");
+                arq.close();
+                gravaarq.close();
+        }
+        arq.close();
+        gravaarq.close();
+    }
+}
